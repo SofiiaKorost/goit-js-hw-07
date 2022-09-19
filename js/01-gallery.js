45cfe61e01b12dc6c0d6a60e7f444017057f5f5a
SofiiaKorost <korostenska@icloud.com>
2022-09-19 21:53:07 +0300

    сhange

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const galleryM = createGalleryM(galleryItems);
+
+
+function createGalerryM(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <div class="gallery__item">
+            <a class="gallery__link"
+                href="${original}">
+                <img
+                class="gallery__image"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}"
+                />
+        </a>
+        </div>`;
+        })
+        .join('');
+}
+
+galleryEl.addEventListener('click', onImageClick);
+
+functio.addEventListener('click', onImgClick);
