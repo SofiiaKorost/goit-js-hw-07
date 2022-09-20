17756dc4db6826f7d06ac931f3ed266dd6a03914
SofiiaKorost <korostenska@icloud.com>
2022-09-20 20:48:16 +0300

    done task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,28 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItMarkup = createGalerryItMarkup(galleryItems);
+
+galleryEl.innerHTML = galleryItMarkup;
+
+function createGalerryItMarkup(galleryIt) {
+    return galleryIt.map(({ preview, original, description }) => {
+        return `
+    <li>
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>
+    `;
+    })
+        .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
